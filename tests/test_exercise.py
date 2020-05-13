3f71b545498e91e1b85ee3b16e2d1d804b5b1a57
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 11:48:03 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -8,7 +8,7 @@ def test_exercise():
     age = 25
     resting_heart_rate = 56
     maximum_heart_rate = 206.3 - (0.711 * age)
-    target_heart_rate = (maximum_heart_rate - resting heart rate) * (target heart rate percentage) + resting heart rate
+    target_heart_rate = (maximum_heart_rate - resting_heart_rate) * 100 + resting_heart_rate
 
     heart = FitByte(age,resting_heart_rate)
 
